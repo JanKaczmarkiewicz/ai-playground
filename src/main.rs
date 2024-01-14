4492e6ce0de78b0618281718b494ed9c43b1ecf6
jkaczmarkiewcz <positiveKiddo@gmail.com>
2024-01-14 22:52:25 +0100

    feat: viz

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
-use aiplay::nn::NeuronNetwork;
+use aiplay::{matrix::ColumnVec, nn::NeuronNetwork, visualize::Visualizer};
 
 fn main() {
-    let data: [(&[f64], &[f64]); 4] = [
-        (&[0.0, 0.0], &[0.0]),
-        (&[0.0, 1.0], &[1.0]),
-        (&[1.0, 0.0], &[0.0]),
-        (&[1.0, 1.0], &[1.0]),
+    let mut data: [(ColumnVec, &[f64]); 4] = [
+        (ColumnVec::from_slice(&[0.0, 0.0]), &[0.0]),
+        (ColumnVec::from_slice(&[0.0, 1.0]), &[1.0]),
+        (ColumnVec::from_slice(&[1.0, 0.0]), &[0.0]),
+        (ColumnVec::from_slice(&[1.0, 1.0]), &[1.0]),
     ];
 
     let layers = [data[0].0.len(), data[0].1.len()];
 
     let mut nn = NeuronNetwork::random(&layers);
 
-    nn.train(&data, 100);
+    let mut visualizer = Visualizer::new().unwrap();
+
+    for _ in 0..1000 {
+        nn.train_step(&mut data);
+        visualizer.draw_nn(&nn);
+
+        println!("{}", nn.cost(&mut data))
+    }
 }
